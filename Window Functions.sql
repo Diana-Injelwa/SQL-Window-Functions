b33f92bcf17be242135eb09e5273b963ac7de051
Diana Injelwa <dianainjelwa@gmail.com>
2024-01-11 14:40:48 +0300

    window clause

--- Window Functions.sql
+++ Window Functions.sql
@@ -2,8 +2,7 @@ USE sql_tutorials;
 
 SELECT * FROM employee;
 -- 1. ROW_NUMBER()
-/* Fetch the first 2 employees to join the company from
-each department */
+-- Fetch the first 2 employees to join the company from each department
 /* To answer this question we assume that emp_id of
 employees who joined earlier is smaller than that of 
 employees who joine later on */ 
@@ -16,7 +15,7 @@ FROM(
 WHERE subquery.rn < 3;
 
 -- 2. RANK()
-/* Fetch top 3 employees earning the maximum salary in each department */
+-- Fetch top 3 employees earning the maximum salary in each department
 SELECT emp_name, department, salary
 FROM(
     SELECT *,
@@ -35,8 +34,7 @@ FROM(
 WHERE subquery.d_rnk < 4;
 
 -- LAG()
-/* Check whether the salary of an employee is higher, lower or equal to 
-the previous employee's salary */
+-- Check whether the salary of an employee is higher, lower or equal to the previous employee's salary
 SELECT emp_id, emp_name, department, salary,
     CASE
         WHEN salary > previous_salary THEN "Higher than previous employee's salary"
@@ -50,8 +48,7 @@ FROM(
 ) subquery;
 
 -- LEAD()
-/* Write a query to check if the salary of an employee is higher,
-equal or lower than the next employee */
+-- Write a query to check if the salary of an employee is higher, equal or lower than the next employee
 SELECT emp_id, emp_name, department, salary,
     CASE
         WHEN salary > next_emp_salary THEN "Higher than next employee's salary"
@@ -65,19 +62,19 @@ FROM(
 ) subquery;
 
 -- FIRST_VALUE()
-/*  What is the most expensive product under each category*/
+-- What is the most expensive product under each category
 SELECT *,
     FIRST_VALUE(product_name) OVER(PARTITION BY product_category ORDER BY price DESC) AS most_expensive_product
 FROM product;
 
 -- LAST_VALUE()
-/* What is the least expensive product under each category */
+-- What is the least expensive product under each category
 /* NOTE: It is important to specify the right Frame Clause when using LAST_VALUE() abd NTH_VALUE()
-    window function so as the function can have access to all the records of 
-    the partitions. The default Frame Clause in MySQL is 
-    "RANGE BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW" which we need to modify
-    to "RANGE BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING" in order to get
-    the correct results set */
+window function so as the function can have access to all the records of 
+the partitions. The default Frame Clause in MySQL is 
+"RANGE BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW" which we need to modify
+to "RANGE BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING" in order to get
+the correct results set */
 SELECT *,
     LAST_VALUE(product_name)
         OVER(PARTITION BY product_category ORDER BY price DESC
@@ -85,7 +82,7 @@ SELECT *,
 FROM product;
 
 -- NTH_VALUE()
-/* What is the 2nd most expensive product under each category */
+-- What is the 2nd most expensive product under each category
 SELECT *,
     NTH_VALUE(product_name, 2)
         OVER(PARTITION BY product_category ORDER BY price DESC
@@ -93,7 +90,7 @@ SELECT *,
 FROM product;
 
 -- NTILE
-/* Group all the Phones into expensive phones, mid range phones and cheaper phones*/
+-- Group all the Phones into expensive phones, mid range phones and cheaper phones
 SELECT product_name, price,
     CASE
         WHEN buckets = 1 THEN 'Expensive'
@@ -108,7 +105,7 @@ FROM(
 ) subquery;
 
 -- Using WINDOW clause in queries
-/* Find the 2nd most expensive product and the least expensive product under each category */
+-- Find the 2nd most expensive product and the least expensive product under each category
 SELECT *,
     NTH_VALUE(product_name, 2) OVER w AS 2nd_most_exp_product,
     LAST_VALUE(product_name) OVER w AS least_expensive_product
